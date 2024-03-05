bfffecda6c4f916220ae0a7e83645ed49fae3a24
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-05 15:59:10 +0100

    refactoring variable

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ submit.addEventListener('click',
 
 
     //inseriamo una costante che sarebbe il prezzo per kilometro
-    const pxkm = 0.21;//number
+    const prezzoperkm = 0.21;//number
         
     //creiamo una variabile contenente il prezzo del biglietto
-    let pxb = (km * pxkm);//number
+    let prezzobiglietto = (km * prezzoperkm);//number
         
     //stampiamo i kilometri da percorrere
     console.log('i km da percorrere sono:',km,'km');
@@ -28,38 +28,38 @@ submit.addEventListener('click',
     //stampiamo l'eta' 
     //console.log('la tua eta e di:',eta,'anni');
 
-    console.log('il biglietto costa =', parseFloat(pxb.toFixed(2)),'euro');
+    console.log('il biglietto costa =', parseFloat(prezzobiglietto.toFixed(2)),'euro');
 
     //utilizziamo delle condizioni per fare degli sconti 
 
     //eta' minore di 18 anni
-    if(eta.value = 'min'){
+    if(eta.value == "Minorenne"){
         
         //pxb20 = variabile contenente il prezzo del biglietto scontato del 20%
-        let pxb20 = pxb * 0.20;//number
+        let sconto20 = prezzobiglietto * 0.20;//number
         
         console.log('Sconto minorenne 20% =', parseFloat(pxb20.toFixed(2)),'euro');
         //pxb20round = variabile contenente il prezzo del biglietto scontato del 20% mostrando fino alla seconda cifra decimale
-        let pxb20round = pxb - pxb20;//number
-        console.log('Totale con sconto applicato =',parseFloat(pxb20round.toFixed(2)),'euro');
+        let sconto20arr = prezzobiglietto - sconto20;//number
+        console.log('Totale con sconto applicato =',parseFloat(sconto20arr.toFixed(2)),'euro');
     
     }//eta' over 65
-    else if(eta.value = 'over'){
+    else if(eta.value == "Over 65"){
        
     //pxb40 = variabile contenente il prezzo del biglietto scontato del 40%
-    let pxb40 = pxb * 0.40;//number
-    console.log('Sconto over 65 40% =', parseFloat(pxb40.toFixed(2)),'euro');
+    let sconto40 = prezzobiglietto * 0.40;//number
+    console.log('Sconto over 65 40% =', parseFloat(sconto40.toFixed(2)),'euro');
 
     //pxb40round = variabile contenente il prezzo del biglietto scontato del 40% mostrando fino alla seconda cifra decimale
-    let pxb40round = pxb - pxb40;//number
-    console.log('Totale con sconto applicato =',parseFloat(pxb40round.toFixed(2)),'euro');
+    let sconto40arr = prezzobiglietto - sconto40;//number
+    console.log('Totale con sconto applicato =',parseFloat(sconto40arr.toFixed(2)),'euro');
 
     }//eta' tra i 18 e i 65 anni
-    else if(eta.value = 'default'){
+    else if(eta.value == "Maggiorenne"){
 
     //stampiamo in console il prezzo del biglietto senza sconti
     console.log('nessuno sconto applicato');
-    console.log('il biglietto costa =', parseFloat(pxb.toFixed(2)),'euro');
+    console.log('il biglietto costa =', parseFloat(prezzobiglietto.toFixed(2)),'euro');
 
     }
 
